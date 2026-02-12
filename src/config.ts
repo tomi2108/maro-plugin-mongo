e09d4f769602bf4b466c16f86407bf6eb9d841c3
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2026-02-12 00:03:05 -0300

    config help

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import z from "zod/v4";
 
 import { ConfigSection } from "../../../dist/lib";
+import { ConfigHelp } from "../../../dist/lib/config/interface";
 
 const schema = z.object({
   secrets: z.array(z.string()).optional()
@@ -12,6 +13,12 @@ export class MigrationsConfig implements ConfigSection {
   validate(config: unknown) {
     if (!config) return {};
     return schema.parse(config);
+  }
+
+  help(): ConfigHelp[] {
+    return [
+      { key: "secrets", description: "Secrets required for MongoDb migrations" }
+    ];
   }
 
   async setup() {
